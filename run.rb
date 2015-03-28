e9b49bc39a3ab896ffa0cf7ca7e77e280d091c79
Rohan Garg <rohan@garg.io>
2015-03-28 01:54:02 +0100

    Config parsing

--- run.rb
+++ run.rb
@@ -3,7 +3,17 @@
 require_relative 'image'
 require_relative 'imageconfig'
 
-c = ImageConfig.new(ARGV[0])
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: run.rb -c CONFIG'
+  opts.on('-cCONFIG', '--config=CONFIG', 'Config file to build') do |c|
+    options[:config] = c
+  end
+end.parse!
+
+c = ImageConfig.new(options[:config])
 
 i = Image.new(c)
 i.run!
