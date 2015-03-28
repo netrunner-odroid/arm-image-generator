7ef55faa397c2c9d62993b1c158e71658e324c7c
Rohan Garg <rohan@garg.io>
2015-03-28 05:56:23 +0100

    We can add a user to multiple groups now

--- rootfs.rb
+++ rootfs.rb
@@ -67,8 +67,10 @@ class RootFS
     system("sudo chroot #{@target} #{pswdcmd}")
     fail 'Could not add the user!' unless $?.success?
 
-    puts 'Adding user to sudo group'
-    system("sudo chroot #{@target} usermod -a -G sudo #{@c.config[:login][:username]}")
-    fail 'Could not add the user to the sudo group!' unless $?.success?
+    @c.config[:login][:groups].each do |g|
+      puts "Adding user to #{g} group"
+      system("sudo chroot #{@target} usermod -a -G #{g} #{@c.config[:login][:username]}")
+      fail 'Could not add the user to the #{g} group!' unless $?.success?
+    end
   end
 end
